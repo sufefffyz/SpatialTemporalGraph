24771060d131fcbba561f10b3626fd7814a692d7
sufefffyz <825521004@qq.com>
2026-04-03 18:07:56 +0800

    Import LargeST source snapshot

---
+++ LargeST/experiments/astgcn/run.sh
@@ -0,0 +1,8 @@
+# python experiments/astgcn/main.py --device cuda:2 --dataset GLA --years 2019 --model_name astgcn --seed 2023 --bs 17 --wdecay 0
+# python experiments/astgcn/main.py --device cuda:2 --dataset GLA --years 2019 --model_name astgcn --seed 2024 --bs 17 --wdecay 0
+
+# python experiments/astgcn/main.py --device cuda:2 --dataset GBA --years 2019 --model_name astgcn --seed 2023 --bs 45
+# python experiments/astgcn/main.py --device cuda:2 --dataset GBA --years 2019 --model_name astgcn --seed 2024 --bs 45
+
+# python experiments/astgcn/main.py --device cuda:2 --dataset SD --years 2019 --model_name astgcn --seed 2023 --bs 64
+# python experiments/astgcn/main.py --device cuda:2 --dataset SD --years 2019 --model_name astgcn --seed 2024 --bs 64
---
+++ LargeST/experiments/d2stgnn/run.sh
@@ -0,0 +1,5 @@
+# python experiments/d2stgnn/main.py --device cuda:2 --dataset GBA --years 2019 --model_name d2stgnn --seed 2023 --max_epochs 80 --patience 80 --bs 4
+# python experiments/d2stgnn/main.py --device cuda:2 --dataset GBA --years 2019 --model_name d2stgnn --seed 2024 --max_epochs 80 --patience 80 --bs 4
+
+# python experiments/d2stgnn/main.py --device cuda:2 --dataset SD --years 2019 --model_name d2stgnn --seed 2023 --max_epochs 80 --patience 80 --bs 45
+# python experiments/d2stgnn/main.py --device cuda:2 --dataset SD --years 2019 --model_name d2stgnn --seed 2024 --max_epochs 80 --patience 80 --bs 45
