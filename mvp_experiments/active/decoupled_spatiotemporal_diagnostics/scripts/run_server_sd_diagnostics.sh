c2b498af65d2d1e801d97abb7aaf5b278e81b77d
sufefffyz <825521004@qq.com>
2026-05-19 10:50:30 +0800

    Narrow SD diagnostics run discovery

--- mvp_experiments/active/decoupled_spatiotemporal_diagnostics/scripts/run_server_sd_diagnostics.sh
+++ mvp_experiments/active/decoupled_spatiotemporal_diagnostics/scripts/run_server_sd_diagnostics.sh
@@ -14,6 +14,7 @@ FFT_CUTOFF_PERIOD="${FFT_CUTOFF_PERIOD:-${MOVING_WINDOW}}"
 DECOMP_METHODS="${DECOMP_METHODS:-moving_average fft_lowpass}"
 PEAK_Q="${PEAK_Q:-0.90}"
 HORIZONS="${HORIZONS:-1 2 3 4 5 6 7 8 9 10 11 12}"
+INCLUDE_TOKENS="${INCLUDE_TOKENS:-${DATASET_NAME}_}"
 
 SEARCH_ROOTS=()
 if [ "$#" -gt 0 ]; then
@@ -32,6 +33,12 @@ for ROOT in "${SEARCH_ROOTS[@]}"; do
 done
 read -r -a DECOMP_METHOD_ARGS <<< "${DECOMP_METHODS}"
 read -r -a HORIZON_ARGS <<< "${HORIZONS}"
+read -r -a INCLUDE_TOKEN_ARGS <<< "${INCLUDE_TOKENS}"
+
+INCLUDE_ARGS=()
+for TOKEN in "${INCLUDE_TOKEN_ARGS[@]}"; do
+  INCLUDE_ARGS+=(--include "${TOKEN}")
+done
 
 python "${SCRIPT_DIR}/run_frequency_diagnostics.py" \
   --dataset-name "${DATASET_NAME}" \
@@ -42,7 +49,7 @@ python "${SCRIPT_DIR}/run_frequency_diagnostics.py" \
   --peak-q "${PEAK_Q}" \
   --horizons "${HORIZON_ARGS[@]}" \
   --max-runs "${MAX_RUNS}" \
-  --include "${DATASET_NAME}" \
+  "${INCLUDE_ARGS[@]}" \
   "${SEARCH_ARGS[@]}"
 
 python "${SCRIPT_DIR}/plot_decoupled_diagnostics.py" \
